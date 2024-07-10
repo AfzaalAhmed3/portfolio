90a07c0d3ddefd1d5388d7f49988f4acf5501e39
AfzaalAhmed3 <afzaalaahmed1@gmail.com>
2024-07-10 21:56:21 +0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,25 @@
+const header = document.querySelector("header");
+
+window.addEventListener("scroll", function() {
+    header.classList.toggle("sticky",this.window.scrollY > 120);
+})
+let menu = document.querySelector("#menu-icon");
+let navlist = document.querySelector('.navlist');
+
+menu.onclick = () =>{
+    menu.classList.toggle('bx-x');
+    navlist.classList.toggle('active');
+}
+
+window.onscroll = () =>{
+    menu.remove.toggle('bx-x');
+    navlist.remove.toggle('active');
+}
+function downloadCV() {
+    const link = document.createElement('a');
+    link.href = 'cv.pdf';
+    link.download = 'cv.pdf';
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+}
